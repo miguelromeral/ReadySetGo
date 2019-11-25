38c779a4d061981cb9d5275494efcfc3826802c2
Miguel Romeral <miguel.romeral@gmail.com>
2019-11-25 18:54:44 +0100

    First Commit Added Settings options in Home fragment. Just the layout so far.

---
+++ app/src/main/java/es/miguelromeral/readysetgo/ui/home/HomeFragment.kt
@@ -0,0 +1,43 @@
+package es.miguelromeral.readysetgo.ui.home
+
+import android.os.Bundle
+import android.view.*
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import es.miguelromeral.readysetgo.R
+
+class HomeFragment : Fragment() {
+
+    private lateinit var homeViewModel: HomeViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        homeViewModel =
+            ViewModelProviders.of(this).get(HomeViewModel::class.java)
+        val root = inflater.inflate(R.layout.fragment_home, container, false)
+        val textView: TextView = root.findViewById(R.id.text_home)
+        homeViewModel.text.observe(this, Observer {
+            textView.text = it
+        })
+
+        setHasOptionsMenu(true)
+
+        return root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?, inflater: MenuInflater?) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater?.inflate(R.menu.options_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        return NavigationUI.onNavDestinationSelected(item!!, view!!.findNavController()) || super.onOptionsItemSelected(item)
+    }
+}
---
+++ app/src/main/java/es/miguelromeral/readysetgo/ui/settings/SettingsViewModel.kt
@@ -0,0 +1,7 @@
+package es.miguelromeral.readysetgo.ui.settings
+
+import androidx.lifecycle.ViewModel
+
+class SettingsViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
