77a680fc6e3501add915c99d0e76a94a27605ed5
Miguel Romeral <miguel.romeral@gmail.com>
2019-11-28 13:42:43 +0100

    Added Logo

--- app/src/main/java/es/miguelromeral/readysetgo/MainActivity.kt
+++ app/src/main/java/es/miguelromeral/readysetgo/MainActivity.kt
@@ -17,8 +17,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        MyApplication.changeStyle()
-
         val navView: BottomNavigationView = findViewById(R.id.nav_view)
 
         val navController = findNavController(R.id.nav_host_fragment)
---
+++ app/src/main/java/es/miguelromeral/readysetgo/ui/splash/SplashScreenActivity.kt
@@ -0,0 +1,29 @@
+package es.miguelromeral.readysetgo.ui.splash
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+import es.miguelromeral.readysetgo.MainActivity
+import es.miguelromeral.readysetgo.MyApplication
+import es.miguelromeral.readysetgo.R
+
+class SplashScreenActivity : AppCompatActivity() {
+    private val SPLASH_TIME_OUT:Long = 2500 // 3 sec
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        MyApplication.changeStyle()
+
+        Handler().postDelayed({
+            // This method will be executed once the timer is over
+            // Start your app main activity
+
+            startActivity(Intent(this, MainActivity::class.java))
+
+            // close this activity
+            finish()
+        }, SPLASH_TIME_OUT)
+    }
+}
